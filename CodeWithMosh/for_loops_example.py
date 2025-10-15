fec965538bc1b29eb90501f2f568bb3513ebdd80
Wachsman, Joshua <joshua.wachsman@radancy.com>
2025-10-15 10:14:22 -0400

    added for loops file with examples inside

---
+++ CodeWithMosh/for_loops_example.py
@@ -0,0 +1,19 @@
+for item in 'Python':
+    print(item)
+print('Done')
+
+for item in ['Josh', 'Cindy', 'Banner', 'Davis', 'Jovi']:
+    print(item)
+print('Done')
+
+for item in range(10):
+    print(item)
+print('Done')
+
+for item in range(5, 10):
+    print(item)
+print('Done')
+
+for item in range(5, 10, 2):  # step size (moves 2 steps forward)
+    print(item)
+print('Done')
