93ee399b8f513955efb7ae30f69e4fbf00ba5bb5
Wachsman, Joshua <joshua.wachsman@radancy.com>
2025-10-16 15:10:07 -0400

    added an example of zipping lists

---
+++ example_zipping.py
@@ -0,0 +1,7 @@
+names = ["Tim", "Joe", "Tom"]
+ages = [25, 30, 35]
+
+zipped = zip(names, ages)
+print(list(zipped))
+# Output: [('Tim', 25), ('Joe', 30), ('Tom', 35)]
+# Note: The zip object can be converted to a list or iterated over directly.
