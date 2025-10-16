204172a4c41ab2ce779229e789fe30f7995d4f0f
Wachsman, Joshua <joshua.wachsman@radancy.com>
2025-10-16 15:32:05 -0400

    added example of list comprehension

--- example_list_comprehension.py
+++ example_list_comprehension.py
@@ -1,2 +1,7 @@
 lst = [x for x in range(10) if x % 2 == 0]
 print(lst)
+# Output: [0, 2, 4, 6, 8]
+
+lst2 = [[x for x in range(10) if x % 2 == 0] for _ in range(5)]
+print(lst2)
+# Output: [[0, 2, 4, 6, 8], [0, 2, 4, 6, 8], [0, 2, 4, 6, 8], [0, 2, 4, 6, 8], [0, 2, 4, 6, 8]]
