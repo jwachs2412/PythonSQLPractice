841c6ce170c757a90ce5b8f165c2384a4a97b90d
Wachsman, Joshua <joshua.wachsman@radancy.com>
2025-10-20 14:59:48 -0400

    added example of print pairs of letters when the letters are different

--- example_two_letter_pairs.py
+++ example_two_letter_pairs.py
@@ -2,6 +2,13 @@ import string
 
 lowercase_alphabet = string.ascii_lowercase
 
+# Print all possible two-letter combinations
 for first_letter in lowercase_alphabet:
     for second_letter in lowercase_alphabet:
         print(first_letter + second_letter)
+
+# Now print only pairs where the letters are different
+for first_letter in lowercase_alphabet:
+    for second_letter in lowercase_alphabet:
+        if first_letter != second_letter:
+            print(first_letter + second_letter)
