2fa1a70eb11dc14518ea479f76e16cf360f51967
Wachsman, Joshua <joshua.wachsman@radancy.com>
2025-10-20 14:13:33 -0400

    Added an additional example to the example_list_comprehension file that prints, sums, and compares all on one line

--- example_list_comprehension.py
+++ example_list_comprehension.py
@@ -5,3 +5,6 @@ print(lst)
 lst2 = [[x for x in range(10) if x % 2 == 0] for _ in range(5)]
 print(lst2)
 # Output: [[0, 2, 4, 6, 8], [0, 2, 4, 6, 8], [0, 2, 4, 6, 8], [0, 2, 4, 6, 8], [0, 2, 4, 6, 8]]
+
+print(sum([i for i in range(1000) if i %
+      3 == 0 or i % 5 == 0]))  # Output: 233168
