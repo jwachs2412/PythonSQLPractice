87c6c5d4c1c2d07e20fef3b4e3cf76c2b6ce3251
Wachsman, Joshua <joshua.wachsman@radancy.com>
2025-10-23 13:41:28 -0400

    added examples of if, elif, else statements

---
+++ CodeYouPractice/practice_if_elif_else.py
@@ -0,0 +1,34 @@
+age = int(input("Please enter your age: "))
+
+if age >= 100:
+    print("You're too old to sign up.")
+elif age >= 18 and age < 100:
+    print("You're now signed up!")
+elif age < 0:
+    print("You haven't been born yet!")
+else:
+    print("You're not eligible. You must be 18+ to sign up.")
+
+
+response = input("Would you like food? (Y/N): ")
+
+if response == "Y":
+    print("Have some food!")
+else:
+    print("No food for you!")
+
+
+name = input("Enter your name: ")
+
+if name == "":
+    print("You did not type in your name!!")
+else:
+    print(f"Hello {name}")
+
+
+for_sale = True
+
+if for_sale:
+    print("This is item is for sale.")
+else:
+    print("This item is not for sale.")
