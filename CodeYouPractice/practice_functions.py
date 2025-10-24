5f3474658b454c1233eb2d17b321eb61c17f8ba2
Wachsman, Joshua <joshua.wachsman@radancy.com>
2025-10-24 13:37:46 -0400

    Ignore nested repo

---
+++ CodeYouPractice/practice_functions.py
@@ -0,0 +1,42 @@
+def first_func():
+    print("Hello World!")
+
+
+first_func()
+
+
+def num_squared(num):
+    print(num**2)
+
+
+num_squared(3)
+num_squared(83)
+num_squared(33)
+
+
+def num_squared_cust(num, power):
+    print(num**power)
+
+
+num_squared_cust(5, 3)
+num_squared_cust(10, 4)
+
+# Arbitrary Args *
+args_tuple = (5, 6, 1, 2, 8)
+
+
+def num_args(*number):
+    print(number[0]*number[1])
+
+
+num_args(*args_tuple)
+
+# Keyword Argument
+
+
+def num_kwarg(**number):
+    print('My number is: ' + number['integer'] +
+          '. My other number: ' + number['integer2'])
+
+
+num_kwarg(integer='2309', integer2='349')
