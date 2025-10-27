0b0ddd3b89c9f0310c13b3704316781bd8e6d9d8
Wachsman, Joshua <joshua.wachsman@radancy.com>
2025-10-27 09:11:30 -0400

    added more for loop examples

--- CodeWithMosh/for_loops_example.py
+++ CodeWithMosh/for_loops_example.py
@@ -6,6 +6,10 @@ for item in ['Josh', 'Cindy', 'Banner', 'Davis', 'Jovi']:
     print(item)
 print('Done')
 
+for item in [1, 2, 3, 5, 343, 3424, 2663, 77342]:
+    print(item)
+print('Done')
+
 for item in range(10):
     print(item)
 print('Done')
@@ -17,3 +21,11 @@ print('Done')
 for item in range(5, 10, 2):  # step size (moves 2 steps forward)
     print(item)
 print('Done')
+
+
+prices = [10, 20, 30]
+total = 0
+
+for price in prices:
+    total += price
+print(f"Total: {total}")
