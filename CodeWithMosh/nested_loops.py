8d58d138f19aafdce629e0b5c1fd96299b617a82
Wachsman, Joshua <joshua.wachsman@radancy.com>
2025-10-27 10:05:27 -0400

    added another nested loop example to file

--- CodeWithMosh/nested_loops.py
+++ CodeWithMosh/nested_loops.py
@@ -4,14 +4,26 @@ for x in range(4):
 
 
 numbers = [5, 2, 5, 2, 2]
+numbers2 = [2, 2, 2, 2, 6]
 
 for x in numbers:
     for y in range(x):  # We could do print("x" * x) but for practicing nested loops we don't want to do that
         print("x", end="")
     print()
 
+for x in numbers2:
+    for y in range(x):  # We could do print("x" * x) but for practicing nested loops we don't want to do that
+        print("x", end="")
+    print()
+
 # Another way to solve
 for x in numbers:
+    output = ''
+    for y in range(x):
+        output += 'x'
+    print(output)
+
+for x in numbers2:
     output = ''
     for y in range(x):
         output += 'x'
